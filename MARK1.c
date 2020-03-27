94307a562ace9521c3e6195e91edbb421926f7b7
Sarthak Mohapatra <sarthakmohapatra88@gmail.com>
2020-03-27 21:30:53 +0530

    mark1.c , problem.txt & solution.txt added

---
+++ MARK1.c
@@ -0,0 +1,9 @@
+/*Ques. Ten students (a,b,c,d,e,f,g,h,i,j) are going to attend an event. 
+There are lots of gift shops, they all are going to the gift shops and 
+randomly picking the gifts. After picking the gifts they are randomly 
+arriving in the billing counter. The accountant gives the preference to 
+that student who has maximum number of gifts. 
+Create a C or Java program to define order of billed students?*/
+ 
+ // case : 1) it gives higher priority to the students who have more no of gifts .
+ //case : 2) if they have same no of gifts the the student(process)having less id is given priority.
