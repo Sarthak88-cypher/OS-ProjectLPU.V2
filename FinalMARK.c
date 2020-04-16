c27e7807e84e25423c049cd6539a1b3607609fdd
Sarthak88-cypher <62335086+Sarthak88-cypher@users.noreply.github.com>
2020-04-16 17:55:02 +0530

    Update FinalMARK.c

--- FinalMARK.c
+++ FinalMARK.c
@@ -1,15 +1,3 @@
-
-/*Ques. Ten students (a,b,c,d,e,f,g,h,i,j) are going to attend an event. 
-There are lots of gift shops, they all are going to the gift shops and 
-randomly picking the gifts. After picking the gifts they are randomly 
-arriving in the billing counter. The accountant gives the preference to 
-that student who has maximum number of gifts. 
-Create a C or Java program to define order of billed students?*/
- 
- // case : 1) it gives higher priority to the students who have more no of gifts .
- //case : 2) if they have same no of gifts the the student(process)having less id is given priority.
-#include <stdio.h> 
-
 	int i,t,j,n;           //variable declaration
 	int B_T[10],p_no[10],bill[10]; //B_T=brust time ,p_no ProcessNumber
 	float cost;
